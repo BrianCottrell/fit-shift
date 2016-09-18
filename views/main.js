7a64be8dd5e901c54d7c8299351e0e7419c4fc54
BrianCottrell <Brian_Cottrell@inbox.com>
2016-09-18 15:09:04 -0700

    Fixed page errors.

--- views/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('username').addEventListener('change', function() {
-	var url = 'http://nodejs-mongodb-example-fit-zoom.0ec9.hackathon.openshiftapps.com/fitness?';
-	document.getElementById('enter').href = url + document.getElementById('username').value;
-});
