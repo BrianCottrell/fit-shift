ba0a41096eab59cb7685fa013b16860d0477a6b1
BrianCottrell <Brian_Cottrell@inbox.com>
2016-09-18 20:16:22 -0700

    Fixed problem with date.

--- server.js
+++ server.js
@@ -58,39 +58,6 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/count', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
 app.get('/', function (req, res) {
   res.render('fitzoom.html');
 });
@@ -115,7 +82,7 @@ app.get('/fitness_data', function (req, res) {
         res.render('fitness_data.html', {data: resultData});
       });
     } else {
-      res.send('Username Not Provided');
+      res.render('fitzoom.html');
     }
   } else {
     res.send('No Database Found');
